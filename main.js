049f3deaad83f8bc406c35bf3704f26bd56cd130
aliabdelhai <ali.abdalhay@mail.huji.ac.il>
2020-11-10 20:24:50 +0200

    apartment-seller

--- main.js
+++ main.js
@@ -6,15 +6,28 @@ $("button").on("click", function () {
     let minRooms = $("#min-r-input").val()
     let maxRooms = $("#max-r-input").val()
     let immediate = $("#immed-y")
+    let parking = $("#park-y")
 
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate, parking)
     renderApts(relevantApts)
 })
 
 const renderApts = function (apartments) {
     $("#results").empty()
     console.log(apartments) //array of apartments to render
-    //Your code goes here.
+    var source = $('#store-template').html();
+    var template = Handlebars.compile(source);
+    const newHTML = template({apartments});
+    $('#results').append(newHTML);
+    if(apartments.length == 0) {
+        var source2 = $('#no-available').html();
+        var template2 = Handlebars.compile(source2);
+        const newHTML2 = template2({str: 'NO APARTMENTS AVAILABLE!'});
+        $('#results').append(newHTML2);
+        
+    }
+
 }
 
 renderApts(apartments) //renders apartments when page loads
+
